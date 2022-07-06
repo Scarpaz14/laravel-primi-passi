e08667f1b3a7e702bcd7bd397c84ae61a87f3c83
Scarpaz14 <scarparofrancesco14@gmail.com>
2022-07-06 19:19:32 +0200

    finito esercizio con bonus

---
+++ resources/views/storia.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>ciao Laravel</h1>
+    <header>
+        <ul class="nav-bar">
+            <li><a href="/" >Home</a></li>
+            <li><a href="/chi-siamo">Chi siamo</a></li>
+            <li><a href="/contatti">Contatti</a></li>
+            <li><a href="/storia">Storia</a></li>
+        </ul>
+    </header>
+    <hr>
+    <h3> STORIA</h3>
+
+    
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/chi-siamo', function () {
+    return view('chi-siamo');
+});
+Route::get('/contatti', function () {
+    return view('contatti');
+});
+Route::get('/storia', function () {
+    return view('storia');
 });
